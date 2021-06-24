f839a202f5aea0270ada60b632576b157a18c4eb
Tuyen Nguyen <trongtuyen96@gmail.com>
2021-06-24 23:32:32 +0700

    Update GET user with conditions (swagger updated)

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -127,7 +127,7 @@ router.get("/", (req, res, next) => {
     let birthDate = req.query.birthDate;
     if (birthDate) {
         condition.push({
-            "birthDate": { $gte: new Date(birthDate) }
+            birthDate: { $gte: new Date(birthDate) }
         });
     }
 
